225274811b1019a3b321e4e66d62422e54d1a8b2
beauberghmans <beauberghmans@gmail.com>
2025-05-09 14:39:17 +0200

    Add files via upload

---
+++ Export-Windows10-Computers (1).ps1
@@ -0,0 +1,19 @@
+# Modules
+Import-Module ActiveDirectory
+
+# Variables
+$twoMonthsInDays = 60
+$twoMonths = (Get-Date).AddDays(-$twoMonthsInDays)
+$domain = "cirt.edu"
+
+# Query AD and add to variable
+$computers = Get-ADComputer -Server $domain -Filter {
+    OperatingSystem -like "*Windows 10*" -and
+    LastLogonDate -ge $twoMonths
+} -Property Name, OperatingSystem, LastLogonDate
+
+# Display results
+$computers | Select-Object Name, OperatingSystem, LastLogonDate | Format-Table -AutoSize
+
+# Export to CSV
+$computers | Select-Object Name, OperatingSystem, LastLogonDate | Export-Csv -Path "c:\Temp\Windows10_computers.csv" -Delimiter ";" -NoTypeInformation
