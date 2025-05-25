ee50ff95f8254bcf7bb81fed067493182c63683a
Beau Berghmansm <beauberghmans@gmail.com>
2025-05-25 17:38:58 +0200

    Refactor: Changed hardcoded value to a parameter

--- Set-NewPassword.ps1
+++ Set-NewPassword.ps1
@@ -5,16 +5,19 @@
     .DESCRIPTION
     Author:Beau Berghmans
 #>
-#Variables
-$remoteComputer = "ADSRV01"
-$creds = Get-Credential -Credential "PersonalLab191\Administrator"
 
+# Parameters
 Param {
     [Parameter(Mandatory=$true)]
-    [string]$user #Fill in samAccountName of user whos password you want to reset
+    [string]$userToResetInAD # Fill in samAccountName of user whos password you want to reset
+    [Parameter(Mandatory=$true)]
+    [string]$authenticatedUser # User account with admin rights in AD
 }
 
-#Generate a random password
+$remoteComputer = "ADSRV01"
+$creds = Get-Credential -Credential "$authenticatedUser"
+
+# Generate a random password
 function Get-RandomPassword {
     param(
             [int]$Length = 12
@@ -24,12 +27,12 @@ function Get-RandomPassword {
 }
 $password = Get-RandomPassword 8
 
-#Connect to remote computer
+# Connect to remote computer
 $newSession = New-PSSession -ComputerName $remoteComputer -Credential $creds
 Import-Module -PSSession $newSession -Name ActiveDirectory
 
-#Set new password
-Set-ADAccountPassword -Identity "$user" -NewPassword (ConvertTo-SecureString "$password" -AsPlainText -Force) -Reset 
+# Set new password
+Set-ADAccountPassword -Identity "$userToResetInAD" -NewPassword (ConvertTo-SecureString "$password" -AsPlainText -Force) -Reset 
 
 Write-Host "The new password is:" -ForegroundColor Blue
 Write-Host "$password" -ForegroundColor Green
